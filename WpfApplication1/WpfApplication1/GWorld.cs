a236e24b9aa10279468321a082f830fb440b1c99
babalida <sasha19961309@gmail.com>
2015-08-13 23:25:51 +0300

    bigg
    bigg

---
+++ WpfApplication1/WpfApplication1/GWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class GWorld
+    {
+        
+        public GWorld(Hero _hero, Enemy[] _enemy, GameMap _gMap)
+        { 
+        
+        }
+    }
+}
